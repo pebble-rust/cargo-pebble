f2a88bcfe325e266171bd5b6b7813bb97f6a33e2
RoccoDev <roccodev.business@gmail.com>
2019-05-27 13:24:04 +0200

    Basic clap structure

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,14 @@
+// Copyright (c) 2019 RoccoDev
+//
+// This software is released under the MIT License.
+// https://opensource.org/licenses/MIT
+
+#[macro_use]
+extern crate clap;
+
+use clap::App;
+
 fn main() {
-    println!("Hello, world!");
+    let yml = load_yaml!("../res/cli.yml");
+    let matches = App::from_yaml(yml).get_matches();
 }
